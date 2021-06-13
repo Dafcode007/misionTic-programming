f5df370b82946d1613e6858af4ca33b4b40edbca
Daniel <dafcode07@gmail.com>
2021-06-13 16:42:01 -0500

    add function char_to_int

--- unal/retos_millo/reto1.py
+++ unal/retos_millo/reto1.py
@@ -4,7 +4,7 @@ def costo_envio(precio_elementos_comprados) -> int:
     if precio_elementos_comprados < 50000:
         return 8000
     elif precio_elementos_comprados >= 50000 and precio_elementos_comprados <= 200000:
-        return precio_elementos_comprados * 3 / 100
+        return int(precio_elementos_comprados * 3 / 100)
     else:
         return 0
 
@@ -15,6 +15,38 @@ def costo_bolsas(cantidad_elementos_comprados:int) -> int:
 
 def precio_total(costo_envio, costo_bolsas, precio_elementos_comprados:int, cantidad_elementos_comprados:int) -> int:
     return precio_elementos_comprados + costo_envio(precio_elementos_comprados) + costo_bolsas(cantidad_elementos_comprados)
+
+def numeros_letras(cantidad_elementos_comprados):
+    unidades = ["cero", "una", "dos" ,"tres" ,"cuatro" ,"cinco" ,
+                "seis" ,"siete" ,"ocho" ,"nueve","diez"]
+    especiales = ["once", "doce","trece","catorce", "quince",
+                "diezciseis", "diecisiete", "dieciocho", "diecinueve"]
+    decenas = ["veinte", "treinta","cuarenta","cincuenta", "sesenta",
+                "setenta", "ochenta", "noventa"]
+
+    #El dato ingresado lo convierte a entero y lo almacena en la variable num
+    num = math.ceil(cantidad_elementos_comprados / 3)
+
+    if (num >=0 and num <11):
+        print(unidades[num])
+
+    elif (num < 20):
+        print(especiales[num-11])
+
+    elif (num <100):
+        
+        unid = num% 10;
+        #math.floor -> obtiene la parte entera de la division num/10
+        #luego ese resultado lo convertimos a entero, 
+        dec = int(math.floor(num/10))
+        if (unid == 0):
+            print(decenas[dec-2]) 
+        else:
+            print(decenas[dec-2], "y" , unidades[unid])
+    else:
+        print("El numero debe ser menor a 100")
+
+
 
 #Entradas
 cantidad_elementos_comprados = int(input())
@@ -29,4 +61,4 @@ print(costo_bolsas(cantidad_elementos_comprados))
 #Precio total
 print(precio_total(costo_envio, costo_bolsas, precio_elementos_comprados, cantidad_elementos_comprados))
 #Cantidad bolsas
-print(math.ceil(cantidad_elementos_comprados / 3))
+numeros_letras(cantidad_elementos_comprados)
